2819d5f9af3cad5aadaffb0adc8e8ab142c0b890
cahein <incidents1371@gmail.com>
2022-08-19 23:38:15 +0900

    20220819

--- 136-SingleNumber.py
+++ 136-SingleNumber.py
@@ -1,5 +1,6 @@
 import collections
+
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        temp =[k for k, v in collections.Counter(nums).items() if v == 1]
-        return temp[0]
+        c = collections.Counter(nums)
+        return c.most_common()[-1][0]
