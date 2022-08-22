a4719f178255ebcc47a6f38e2fb836a955585eae
cahein <incidents1371@gmail.com>
2022-08-22 13:52:17 +0900

    20220822

--- 338-CountingBits.py
+++ 338-CountingBits.py
@@ -1,4 +1,9 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
-        temp = [bin(i).count("1") for i in range(n+1)]
-        return temp
+        ans = []
+        for i in range(n+1):
+            #print(bin(i)[2:])
+            #print(sum(list(map(int, str(bin(i)[2:])))))
+            ans.append(sum(list(map(int, str(bin(i)[2:])))))
+        return ans
+        
