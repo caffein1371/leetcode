2b8ebeb21519bd9170a750e10d8557351f5737fb
cahein <incidents1371@gmail.com>
2022-08-22 13:58:21 +0900

    20220822

--- 338-CountingBits.py
+++ 338-CountingBits.py
@@ -1,9 +1,14 @@
 class Solution:
     def countBits(self, n: int) -> List[int]:
+        def digit_sum(n):
+            # 各桁の和を求める
+            # 計算量: O(logN)
+            ans = 0
+            while n > 0:
+                ans += n % 10
+                n //= 10
+            return ans
         ans = []
         for i in range(n+1):
-            #print(bin(i)[2:])
-            #print(sum(list(map(int, str(bin(i)[2:])))))
-            ans.append(sum(list(map(int, str(bin(i)[2:])))))
+            ans.append(digit_sum(int(bin(i)[2:])))
         return ans
-        
