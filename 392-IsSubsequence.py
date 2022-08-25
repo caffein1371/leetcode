c7e0fecb03e4730c5c4ff95d512e6b5ec048933f
cahein <incidents1371@gmail.com>
2022-08-25 11:06:18 +0900

    20220825

--- 392-IsSubsequence.py
+++ 392-IsSubsequence.py
@@ -1,16 +1,14 @@
 class Solution:
     def isSubsequence(self, s: str, t: str) -> bool:
-        s = list(s)
-        t = list(t)
-        if len(s)==0:
+        if len(s) == 0 and len(t) == 0:
             return True
         for i in range(len(t)):
-            print (t)
-            print (s)
+            print (t[i])
             if t[i] in s and len(s)!=0:
-                s.remove(t[i])
+                s=s.removeprefix(t[i])
+            print (s)
             if len(s)==0:
                 return True
-            
         if len(s)!=0:
-            return False
+                return False
+                    
