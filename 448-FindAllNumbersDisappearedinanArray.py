49c5cf4639cbac0f0b49a2419ec0010f041ab237
cahein <incidents1371@gmail.com>
2022-08-26 11:38:21 +0900

    20220826

--- 448-FindAllNumbersDisappearedinanArray.py
+++ 448-FindAllNumbersDisappearedinanArray.py
@@ -1,9 +1,7 @@
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        orinum = len(nums)
-        nums= set(sorted(nums))
-        ans = []
-        for i in range(1,orinum+1):
-            if i not in nums:
-                ans.append(i)
-        return ans
+        nums = sorted(nums)
+        temp = [i for i in range(1,len(nums)+1)]
+        ans = set(nums)^set(temp)        
+            
+        return list(ans)
